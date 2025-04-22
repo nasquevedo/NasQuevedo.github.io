d089b54d0653a64c1a0fab5cd81204c13282f596
nasquevedo <qesantiago@gmail.com>
2025-04-21 20:06:16 -0500

    Updated to show a more formal website

---
+++ index.js
@@ -0,0 +1,44 @@
+var lang = 'en';
+if (localStorage.getItem('lang') === undefined || localStorage.getItem('lang') === null) {
+    localStorage.setItem('lang', lang);
+} else {
+    lang = localStorage.getItem('lang');
+}
+
+const activeLink = (id) => {
+    jQuery(".nav-main-link").removeClass('active');
+
+    jQuery("#" + id).addClass('active');
+}
+
+const titles = async () => {
+    try {
+        const response = await fetch(`./lang/${lang}.json`);
+        const result = await response.json();
+
+        result.data.forEach(item => {
+            jQuery("#" +  item.id).html(item.value);
+        });
+    } catch(e) {
+        console.log("Error trying to get the titles", e);
+    }
+};
+
+const changeLang = (value) => {
+    localStorage.setItem('lang', value);
+    lang = value;
+
+    html = "<img class='icons' src='./img/icons/estados-unidos.png' alt='USA flag'/>";
+    html += " EN" 
+    jQuery("#current-lang").html(html);
+    if (value === 'es') {
+        html = "<img class='icons' src='./img/icons/colombia.png' alt='usa flag'/>";
+        html += " ES"
+        jQuery("#current-lang").html(html);
+    }
+
+    titles();
+    languages();
+};
+
+changeLang(localStorage.getItem('lang'));
--- js/global.js
+++
@@ -1,6 +0,0 @@
-var lang = 'en';
-if (localStorage.getItem('lang') === undefined || localStorage.getItem('lang') === null) {
-    localStorage.setItem('lang', lang);
-} else {
-    lang = localStorage.getItem('lang');
-}
